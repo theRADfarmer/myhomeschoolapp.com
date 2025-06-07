3e75a6725423049310291c8b5e1e6ca4ca9b552c
theRADfarmer <theradfarmer@gmail.com>
2025-06-07 15:23:06 -0500

    feat: Add detail views for Student, Subject, and Assignment models ie. added PUT, PATCH, and DELETE capabilities

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,14 +4,29 @@ from subjects.models import Subject
 from assignments.models import Assignment
 from .serializers import StudentSerializer, SubjectSerializer, AssignmentSerializer
 
+# Student
 class StudentListCreateView(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+# Subject
 class SubjectListCreateView(generics.ListCreateAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+# Assignment
 class AssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
+
+class AssignmentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Assignment.objects.all()
     serializer_class = AssignmentSerializer
