149be9ef0826951403ee95c71bf86578495ae10f
theRADfarmer <theradfarmer@gmail.com>
2025-06-07 09:50:07 -0500

    fix: Changed Subject model relationship to align with spec.

--- backend/subjects/admin.py
+++ backend/subjects/admin.py
@@ -3,6 +3,6 @@ from .models import Subject
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ("name", "user")
-    list_filter = ("user",)
-    search_fields = ("name", "user__email")
+    list_display = ("name", "student__first_name")
+    list_filter = ("student",)
+    search_fields = ("name", "student__first_name")
--- backend/subjects/models.py
+++ backend/subjects/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from students.models import Student
 
 class Subject(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subjects')
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='subjects')
     name = models.CharField(max_length=100)
     course_description = models.TextField(max_length=1000, blank=True, null=True)
     notes = models.TextField(max_length=1000, blank=True, null=True)
