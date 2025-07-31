a173b2b869995a8d03bcde1ce51bd715f12d6847
theRADfarmer <theradfarmer@gmail.com>
2025-07-31 18:08:31 -0500

    Configure whitenoise

--- backend/myhomeschoolapp/settings.py
+++ backend/myhomeschoolapp/settings.py
@@ -156,6 +156,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
