ce24695c66e0085c534633642c05486b3aefac2f
theRADfarmer <theradfarmer@gmail.com>
2025-08-01 09:04:16 -0500

    Fixed "invalid clerk token".

--- backend/api/clerk_auth.py
+++ backend/api/clerk_auth.py
@@ -1,16 +1,17 @@
 import requests
 import jwt
-from decouple import config
 from rest_framework import authentication, exceptions
 from django.conf import settings
 
 CLERK_JWKS_URL = settings.CLERK_JWKS_URL
-CLERK_CLIENT_ID = settings.CLERK_AUDIENCE
+CLERK_AUDIENCE = settings.CLERK_AUDIENCE
+CLERK_ISSUER = settings.CLERK_ISSUER  # e.g., "https://clerk.myhomeschoolapp.com"
 
 class ClerkUser:
     def __init__(self, clerk_user_id):
         self.clerk_user_id = clerk_user_id
         self.is_authenticated = True
+
     def __str__(self):
         return self.clerk_user_id
 
@@ -21,16 +22,33 @@ class ClerkAuthentication(authentication.BaseAuthentication):
             return None
 
         token = auth_header.split(' ')[1]
+
         try:
-            jwks = requests.get(CLERK_JWKS_URL).json()
-            public_keys = {key['kid']: jwt.algorithms.RSAAlgorithm.from_jwk(key) for key in jwks['keys']}
+            jwks = requests.get(CLERK_JWKS_URL, timeout=5).json()
+            public_keys = {
+                key['kid']: jwt.algorithms.RSAAlgorithm.from_jwk(key)
+                for key in jwks['keys']
+            }
+
             unverified_header = jwt.get_unverified_header(token)
-            key = public_keys[unverified_header['kid']]
-            payload = jwt.decode(token, key=key, algorithms=['RS256'], audience=CLERK_CLIENT_ID)
+            kid = unverified_header.get('kid')
+            key = public_keys.get(kid)
+
+            if not key:
+                raise exceptions.AuthenticationFailed('Invalid Clerk key ID')
+
+            payload = jwt.decode(
+                token,
+                key=key,
+                algorithms=['RS256'],
+                audience=CLERK_AUDIENCE,
+                issuer=CLERK_ISSUER
+            )
+
             clerk_user_id = payload['sub']
+
         except Exception as ex:
             print(ex)
             raise exceptions.AuthenticationFailed('Invalid Clerk token')
 
         return (ClerkUser(clerk_user_id), None)
-
